deecbe99c57ec77269b05e0a3fd78646ee72e73a
luchocampos <lucho.campos@outlook.com>
2020-04-22 21:40:59 -0300

    ultimo

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -46,6 +46,10 @@ var app = new Framework7({
         path: '/inicioforo/',
         url: 'inicioforo.html',
       },
+      {
+        path: '/forogeneral/',
+        url: 'forogeneral.html',
+      },
 
 
 
@@ -86,7 +90,7 @@ $$(document).on('page:init', function (e) {
       $$('#btn-register').on('click', fnRegister);
       $$('.next').on('click', fnNext);
       $$('.btn-google').on('click', fnGoogle);
-
+      
 
 })
 
@@ -122,43 +126,6 @@ spaceBetween:0,
 
 }
 );
-var items = [];
-for (var i = 1; i <= 10000; i++) {
-  items.push({
-    title: 'Item ' + i,
-    subtitle: 'Subtitle ' + i
-  });
-}
-
-var virtualList = app.virtualList.create({
-  // List Element
-  el: '.virtual-list',
-  // Pass array with items
-  items: items,
-  // Custom search function for searchbar
-  searchAll: function (query, items) {
-    var found = [];
-    for (var i = 0; i < items.length; i++) {
-      if (items[i].title.toLowerCase().indexOf(query.toLowerCase()) >= 0 || query.trim() === '') found.push(i);
-    }
-    return found; //return array with mathced indexes
-  },
-  // List item Template7 template
-  itemTemplate:
-    '<li>' +
-      '<a href="#" class="item-link item-content">' +
-        '<div class="item-inner">' +
-          '<div class="item-title-row">' +
-            '<div class="item-title">{{title}}</div>' +
-          '</div>' +
-          '<div class="item-subtitle">{{subtitle}}</div>' +
-        '</div>' +
-      '</a>' +
-    '</li>',
-  // Item height
-  height: app.theme === 'ios' ? 63 : (app.theme === 'md' ? 73 : 46),
-});
-
 
 
 
